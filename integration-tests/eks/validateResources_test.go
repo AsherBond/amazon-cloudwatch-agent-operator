769b98f80a9eede89bc2f699e68939f4e3394e85
siprmp <siprmp@amazon.com>
2024-03-14 09:32:45 -0400

    fixing index out of bounds issues and how many services tested

--- integration-tests/eks/validateResources_test.go
+++ integration-tests/eks/validateResources_test.go
@@ -32,7 +32,7 @@ const (
 	addOnName        = "amazon-cloudwatch-observability"
 	agentName        = "cloudwatch-agent"
 	podNameRegex     = "(" + agentName + "|" + addOnName + "-controller-manager|fluent-bit)-*"
-	serviceNameRegex = agentName + "(-headless|-monitoring)?|" + addOnName + "-webhook-service|" + "dcgm-exporter"
+	serviceNameRegex = agentName + "(-headless|-monitoring)?|" + addOnName + "-webhook-service|" + "dcgm-exporter-service"
 )
 
 func TestOperatorOnEKs(t *testing.T) {
@@ -78,7 +78,7 @@ func TestOperatorOnEKs(t *testing.T) {
 	//Validating the services
 	services, err := ListServices(nameSpace, clientSet)
 	assert.NoError(t, err)
-	assert.Len(t, services.Items, 4)
+	assert.Len(t, services.Items, 5)
 	for _, service := range services.Items {
 		fmt.Println("service name: " + service.Name + " namespace:" + service.Namespace)
 		// matches
@@ -124,12 +124,17 @@ func TestOperatorOnEKs(t *testing.T) {
 		fmt.Println("Error:", err)
 		return
 	}
+
+	fmt.Println("This is hte index of annotation: ", indexOfAutoAnnotationConfigString)
 	//if auto annotation not part of config, we will add it
-	if indexOfAutoAnnotationConfigString == -1 {
+	if indexOfAutoAnnotationConfigString < 0 || indexOfAutoAnnotationConfigString >= len(deployments.Items[0].Spec.Template.Spec.Containers[0].Args) {
+		fmt.Println("We are in the if statement")
 		deployments.Items[0].Spec.Template.Spec.Containers[0].Args = append(deployments.Items[0].Spec.Template.Spec.Containers[0].Args, "--auto-annotation-config="+string(jsonStr))
 		fmt.Println("AutoAnnotationConfiguration: " + deployments.Items[0].Spec.Template.Spec.Containers[0].Args[indexOfAutoAnnotationConfigString])
-
+		indexOfAutoAnnotationConfigString = len(deployments.Items[0].Spec.Template.Spec.Containers[0].Args) - 1
+		fmt.Println("This is the updated index of annotation: ", indexOfAutoAnnotationConfigString)
 	} else {
+		fmt.Println("We are in the else statement")
 		deployments.Items[0].Spec.Template.Spec.Containers[0].Args[indexOfAutoAnnotationConfigString] = "--auto-annotation-config=" + string(jsonStr)
 		fmt.Println("AutoAnnotationConfiguration: " + deployments.Items[0].Spec.Template.Spec.Containers[0].Args[indexOfAutoAnnotationConfigString])
 
