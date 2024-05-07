531fb15c91f45c251aa24c896a2e5e74712dd9d9
Paramadon <siprmp@amazon.com>
2024-05-07 15:59:47 -0400

    adding test

--- integration-tests/terraform/basic_components/output.tf
+++ integration-tests/terraform/basic_components/output.tf
@@ -16,7 +16,3 @@ output "public_subnet_ids" {
 output "role_arn" {
   value = data.aws_iam_role.cwagent_iam_role.arn
 }
-
-output "instance_profile" {
-  value = data.aws_iam_instance_profile.cwagent_instance_profile.name
-}
