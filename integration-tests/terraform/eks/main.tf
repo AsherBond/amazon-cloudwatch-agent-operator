5d0341901b63b47073eb283db430b4436e89db76
Paramadon <siprmp@amazon.com>
2024-05-07 11:44:30 -0400

    change instance type

--- integration-tests/terraform/eks/main.tf
+++ integration-tests/terraform/eks/main.tf
@@ -44,7 +44,7 @@ resource "aws_eks_node_group" "this" {
   ami_type       = "AL2_x86_64"
   capacity_type  = "ON_DEMAND"
   disk_size      = 20
-  instance_types = ["t3.medium"]
+  instance_types = ["g4dn.xlarge"]
 
   depends_on = [
     aws_iam_role_policy_attachment.node_AmazonEC2ContainerRegistryReadOnly,
