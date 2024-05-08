d788bd3e9adb0bcc1558f121a0e47305f52cbfb7
Paramadon <siprmp@amazon.com>
2024-05-08 14:27:58 -0400

    adding test

--- integration-tests/terraform/eks/main.tf
+++ integration-tests/terraform/eks/main.tf
@@ -13,6 +13,7 @@ module "basic_components" {
   region = var.region
 }
 
+
 data "aws_eks_cluster_auth" "this" {
   name = aws_eks_cluster.this.name
 }
@@ -34,6 +35,31 @@ resource "aws_eks_cluster" "this" {
   }
 }
 
+# EKS Node Groups
+resource "aws_eks_node_group" "this" {
+  cluster_name    = aws_eks_cluster.this.name
+  node_group_name = "cwagent-eks-integ-node"
+  node_role_arn   = aws_iam_role.node_role.arn
+  subnet_ids      = module.basic_components.public_subnet_ids
+
+  scaling_config {
+    desired_size = 1
+    max_size     = 1
+    min_size     = 1
+  }
+
+  ami_type       = var.ami_type
+  capacity_type  = "ON_DEMAND"
+  disk_size      = 20
+  instance_types = [var.instance_type]
+
+  depends_on = [
+    aws_iam_role_policy_attachment.node_AmazonEC2ContainerRegistryReadOnly,
+    aws_iam_role_policy_attachment.node_AmazonEKS_CNI_Policy,
+    aws_iam_role_policy_attachment.node_AmazonEKSWorkerNodePolicy,
+    aws_iam_role_policy_attachment.node_CloudWatchAgentServerPolicy
+  ]
+}
 
 # EKS Node IAM Role
 resource "aws_iam_role" "node_role" {
@@ -80,6 +106,28 @@ resource "aws_security_group" "eks_cluster_sg" {
   description = "Cluster communication with worker nodes"
   vpc_id      = module.basic_components.vpc_id
 }
+
+resource "aws_security_group_rule" "cluster_inbound" {
+  description              = "Allow worker nodes to communicate with the cluster API Server"
+  from_port                = 443
+  protocol                 = "tcp"
+  security_group_id        = aws_security_group.eks_cluster_sg.id
+  source_security_group_id = aws_security_group.eks_nodes_sg.id
+  to_port                  = 443
+  type                     = "ingress"
+}
+
+resource "aws_security_group_rule" "cluster_outbound" {
+  description              = "Allow cluster API Server to communicate with the worker nodes"
+  from_port                = 1024
+  protocol                 = "tcp"
+  security_group_id        = aws_security_group.eks_cluster_sg.id
+  source_security_group_id = aws_security_group.eks_nodes_sg.id
+  to_port                  = 65535
+  type                     = "egress"
+}
+
+
 # EKS Node Security Group
 resource "aws_security_group" "eks_nodes_sg" {
   name        = "cwagent-eks-node-sg-${module.common.testing_id}"
@@ -115,52 +163,86 @@ resource "aws_security_group_rule" "nodes_cluster_inbound" {
 }
 
 
-resource "aws_security_group_rule" "cluster_inbound" {
-  description              = "Allow worker nodes to communicate with the cluster API Server"
-  from_port                = 443
-  protocol                 = "tcp"
-  security_group_id        = aws_security_group.eks_cluster_sg.id
-  source_security_group_id = aws_security_group.eks_nodes_sg.id
-  to_port                  = 443
-  type                     = "ingress"
+# create cert for communication between agent and dcgm
+resource "tls_private_key" "private_key" {
+  algorithm = "RSA"
 }
 
-resource "aws_security_group_rule" "cluster_outbound" {
-  description              = "Allow cluster API Server to communicate with the worker nodes"
-  from_port                = 1024
-  protocol                 = "tcp"
-  security_group_id        = aws_security_group.eks_cluster_sg.id
-  source_security_group_id = aws_security_group.eks_nodes_sg.id
-  to_port                  = 65535
-  type                     = "egress"
+resource "local_file" "ca_key" {
+  content  = tls_private_key.private_key.private_key_pem
+  filename = "${path.module}/certs/ca.key"
 }
 
-# EKS Node Groups
-resource "aws_eks_node_group" "this" {
-  cluster_name    = aws_eks_cluster.this.name
-  node_group_name = "cwagent-operator-eks-integ-node"
-  node_role_arn   = aws_iam_role.node_role.arn
-  subnet_ids      = module.basic_components.public_subnet_ids
-
-  scaling_config {
-    desired_size = 1
-    max_size     = 1
-    min_size     = 1
+resource "tls_self_signed_cert" "ca_cert" {
+  private_key_pem   = tls_private_key.private_key.private_key_pem
+  is_ca_certificate = true
+  subject {
+    common_name  = "dcgm-exporter-service.amazon-cloudwatch.svc"
+    organization = "Amazon CloudWatch Agent"
   }
-
-  ami_type       = "AL2_x86_64"
-  capacity_type  = "ON_DEMAND"
-  disk_size      = 20
-  instance_types = ["t3.medium"]
-
-  depends_on = [
-    aws_iam_role_policy_attachment.node_AmazonEC2ContainerRegistryReadOnly,
-    aws_iam_role_policy_attachment.node_AmazonEKS_CNI_Policy,
-    aws_iam_role_policy_attachment.node_AmazonEKSWorkerNodePolicy,
-    aws_iam_role_policy_attachment.node_CloudWatchAgentServerPolicy
+  validity_period_hours = 24
+  allowed_uses = [
+    "digital_signature",
+    "key_encipherment",
+    "cert_signing",
+    "crl_signing",
+    "server_auth",
+    "client_auth",
   ]
 }
 
+resource "local_file" "ca_cert_file" {
+  content  = tls_self_signed_cert.ca_cert.cert_pem
+  filename = "${path.module}/certs/ca.cert"
+}
+
+resource "tls_private_key" "server_private_key" {
+  algorithm = "RSA"
+}
+
+resource "local_file" "server_key" {
+  content  = tls_private_key.server_private_key.private_key_pem
+  filename = "${path.module}/certs/server.key"
+}
+
+resource "tls_cert_request" "local_csr" {
+  private_key_pem = tls_private_key.server_private_key.private_key_pem
+  dns_names       = ["localhost", "127.0.0.1", "dcgm-exporter-service.amazon-cloudwatch.svc"]
+  subject {
+    common_name  = "dcgm-exporter-service.amazon-cloudwatch.svc"
+    organization = "Amazon CloudWatch Agent"
+  }
+}
+
+resource "tls_locally_signed_cert" "server_cert" {
+  cert_request_pem      = tls_cert_request.local_csr.cert_request_pem
+  ca_private_key_pem    = tls_private_key.private_key.private_key_pem
+  ca_cert_pem           = tls_self_signed_cert.ca_cert.cert_pem
+  validity_period_hours = 12
+  allowed_uses = [
+    "digital_signature",
+    "key_encipherment",
+    "server_auth",
+    "client_auth",
+  ]
+}
+
+resource "local_file" "server_cert_file" {
+  content  = tls_locally_signed_cert.server_cert.cert_pem
+  filename = "${path.module}/certs/server.cert"
+}
+
+resource "kubernetes_secret" "agent_cert" {
+  metadata {
+    name      = "amazon-cloudwatch-observability-agent-cert"
+    namespace = "amazon-cloudwatch"
+  }
+  data = {
+    "ca.crt"  = tls_self_signed_cert.ca_cert.cert_pem              #filebase64(local_file.ca_cert_file.filename)
+    "tls.crt" = tls_locally_signed_cert.server_cert.cert_pem       #filebase64(local_file.server_cert_file.filename)
+    "tls.key" = tls_private_key.server_private_key.private_key_pem #filebase64(local_file.server_key.filename)
+  }
+}
 
 
 resource "kubernetes_namespace" "namespace" {
@@ -168,32 +250,341 @@ resource "kubernetes_namespace" "namespace" {
     name = "amazon-cloudwatch"
   }
 }
-resource "null_resource" "kubectl" {
+
+# dummy daemonset that simulates dcgm-exporter assuming there is only 1 GPU available
+resource "kubernetes_daemonset" "exporter" {
   depends_on = [
-    aws_eks_cluster.this,
-    aws_eks_node_group.this
+    kubernetes_namespace.namespace,
+    kubernetes_service_account.cwagentservice,
+    aws_eks_node_group.this,
+    kubernetes_config_map.httpdconfig,
   ]
-  provisioner "local-exec" {
-    command = <<-EOT
-      ${local.aws_eks} update-kubeconfig --name ${aws_eks_cluster.this.name}
-      ${local.aws_eks} list-clusters --output text
-      ${local.aws_eks} describe-cluster --name ${aws_eks_cluster.this.name} --output text
-    EOT
+  metadata {
+    name      = "dcgm-exporter"
+    namespace = "amazon-cloudwatch"
+    labels = {
+      k8s-app = "dcgm-exporter"
+    }
+  }
+  spec {
+    selector {
+      match_labels = {
+        "k8s-app" = "dcgm-exporter"
+      }
+    }
+    template {
+      metadata {
+        labels = {
+          "name" : "dcgm-exporter"
+          "k8s-app" : "dcgm-exporter"
+        }
+      }
+      spec {
+        node_selector = {
+          "kubernetes.io/os" : "linux"
+        }
+        container {
+          name  = "dcgm-exporter"
+          image = "httpd:2.4-alpine"
+          resources {
+            limits = {
+              "cpu" : "50m",
+              "memory" : "50Mi"
+            }
+            requests = {
+              "cpu" : "50m",
+              "memory" : "50Mi"
+            }
+          }
+          port {
+            name           = "metrics"
+            container_port = 9400
+            host_port      = 9400
+            protocol       = "TCP"
+          }
+          command = [
+            "/bin/sh",
+            "-c",
+          ]
+          args = [
+            "/bin/echo 'DCGM_FI_DEV_GPU_UTIL{PodName=\"pod1\",gpu=\"0\",UUID=\"uuid0\",device=\"nvidia0\",modelName=\"Tesla T4\",Hostname=\"hostname1\",container=\"main\",namespace=\"amazon-cloudwatch\",pod=\"pod1-hash\"} 1\nDCGM_FI_DEV_FB_FREE{PodName=\"pod1\",gpu=\"0\",UUID=\"uuid0\",device=\"nvidia0\",modelName=\"Tesla T4\",Hostname=\"hostname1\",container=\"main\",namespace=\"amazon-cloudwatch\",pod=\"pod1-hash\"} 1\nDCGM_FI_DEV_FB_USED{PodName=\"pod1\",gpu=\"0\",UUID=\"uuid0\",device=\"nvidia0\",modelName=\"Tesla T4\",Hostname=\"hostname1\",container=\"main\",namespace=\"amazon-cloudwatch\",pod=\"pod1-hash\"} 1\nDCGM_FI_DEV_FB_TOTAL{PodName=\"pod1\",gpu=\"0\",UUID=\"uuid0\",device=\"nvidia0\",modelName=\"Tesla T4\",Hostname=\"hostname1\",container=\"main\",namespace=\"amazon-cloudwatch\",pod=\"pod1-hash\"} 1\nDCGM_FI_DEV_FB_USED_PERCENT{PodName=\"pod1\",gpu=\"0\",UUID=\"uuid0\",device=\"nvidia0\",modelName=\"Tesla T4\",Hostname=\"hostname1\",container=\"main\",namespace=\"amazon-cloudwatch\",pod=\"pod1-hash\"} 1\nDCGM_FI_DEV_GPU_TEMP{PodName=\"pod1\",gpu=\"0\",UUID=\"uuid0\",device=\"nvidia0\",modelName=\"Tesla T4\",Hostname=\"hostname1\",container=\"main\",namespace=\"amazon-cloudwatch\",pod=\"pod1-hash\"} 1\nDCGM_FI_DEV_POWER_USAGE{PodName=\"pod1\",gpu=\"0\",UUID=\"uuid0\",device=\"nvidia0\",modelName=\"Tesla T4\",Hostname=\"hostname1\",container=\"main\",namespace=\"amazon-cloudwatch\",pod=\"pod1-hash\"} 1' >> /usr/local/apache2/htdocs/metrics && sed -i -e \"s/hostname1/$HOST_NAME/g\" /usr/local/apache2/htdocs/metrics && httpd-foreground -k restart"
+          ]
+          volume_mount {
+            mount_path = "/etc/amazon-cloudwatch-observability-dcgm-cert"
+            name       = "dcgmtls"
+            read_only  = true
+          }
+          volume_mount {
+            mount_path = "/usr/local/apache2/conf/httpd.conf"
+            sub_path   = "httpd.conf"
+            name       = "httpdconfig"
+            read_only  = true
+          }
+          volume_mount {
+            mount_path = "/usr/local/apache2/conf/extra/httpd-ssl.conf"
+            sub_path   = "httpd-ssl.conf"
+            name       = "httpdconfig"
+            read_only  = true
+          }
+          env {
+            name = "HOST_IP"
+            value_from {
+              field_ref {
+                field_path = "status.hostIP"
+              }
+            }
+          }
+          env {
+            name = "HOST_NAME"
+            value_from {
+              field_ref {
+                field_path = "spec.nodeName"
+              }
+            }
+          }
+          env {
+            name = "K8S_NAMESPACE"
+            value_from {
+              field_ref {
+                field_path = "metadata.namespace"
+              }
+            }
+          }
+        }
+        volume {
+          name = "dcgmtls"
+          secret {
+            secret_name = "amazon-cloudwatch-observability-agent-cert"
+            items {
+              key  = "tls.crt"
+              path = "server.crt"
+            }
+            items {
+              key  = "tls.key"
+              path = "server.key"
+            }
+          }
+        }
+        volume {
+          name = "httpdconfig"
+          config_map {
+            name = "httpdconfig"
+          }
+        }
+        service_account_name             = "cloudwatch-agent"
+        termination_grace_period_seconds = 60
+      }
+    }
   }
 }
-locals {
-   cwagent_config   = fileexists("${var.test_dir}/resources/config.json") ? "${var.test_dir}/resources/config.json" : "../default_resources/default_amazon_cloudwatch_agent.json"
-   aws_eks  = format("%s%s", "aws eks --region ${var.region}", var.beta ? " --endpoint ${var.beta_endpoint}" : "")
- }
 
-
-resource "aws_eks_addon" "this" {
+resource "kubernetes_service" "exporter" {
   depends_on = [
-    null_resource.kubectl
+    kubernetes_namespace.namespace,
+    kubernetes_service_account.cwagentservice,
+    aws_eks_node_group.this,
+    kubernetes_daemonset.exporter
   ]
-  addon_name   = var.addon_name
-  cluster_name = aws_eks_cluster.this.name
-  addon_version = var.addon_version
+  metadata {
+    name      = "dcgm-exporter-service"
+    namespace = "amazon-cloudwatch"
+    labels = {
+      "k8s-app" : "dcgm-exporter-service"
+    }
+    annotations = {
+      "prometheus.io/scrape" : "true"
+    }
+  }
+  spec {
+    type = "ClusterIP"
+    selector = {
+      k8s-app = "dcgm-exporter"
+    }
+    port {
+      name        = "metrics"
+      port        = 9400
+      target_port = 9400
+      protocol    = "TCP"
+    }
+  }
+}
+
+resource "kubernetes_daemonset" "service" {
+  depends_on = [
+    kubernetes_namespace.namespace,
+    kubernetes_service_account.cwagentservice,
+    aws_eks_node_group.this,
+    kubernetes_service.exporter
+  ]
+  metadata {
+    name      = "cloudwatch-agent"
+    namespace = "amazon-cloudwatch"
+  }
+  spec {
+    selector {
+      match_labels = {
+        "name" : "cloudwatch-agent"
+      }
+    }
+    template {
+      metadata {
+        labels = {
+          "name" : "cloudwatch-agent"
+        }
+      }
+      spec {
+        node_selector = {
+          "kubernetes.io/os" : "linux"
+        }
+        container {
+          name              = "cwagent"
+          image             = "${var.cwagent_image_repo}:${var.cwagent_image_tag}"
+          image_pull_policy = "Always"
+          resources {
+            limits = {
+              "cpu" : "200m",
+              "memory" : "200Mi"
+            }
+            requests = {
+              "cpu" : "200m",
+              "memory" : "200Mi"
+            }
+          }
+          port {
+            container_port = 25888
+            host_port      = 25888
+            protocol       = "UDP"
+          }
+          env {
+            name = "HOST_IP"
+            value_from {
+              field_ref {
+                field_path = "status.hostIP"
+              }
+            }
+          }
+          env {
+            name = "HOST_NAME"
+            value_from {
+              field_ref {
+                field_path = "spec.nodeName"
+              }
+            }
+          }
+          env {
+            name = "K8S_NAMESPACE"
+            value_from {
+              field_ref {
+                field_path = "metadata.namespace"
+              }
+            }
+          }
+          volume_mount {
+            mount_path = "/etc/cwagentconfig"
+            name       = "cwagentconfig"
+          }
+          volume_mount {
+            mount_path = "/rootfs"
+            name       = "rootfs"
+            read_only  = true
+          }
+          volume_mount {
+            mount_path = "/var/run/docker.sock"
+            name       = "dockersock"
+            read_only  = true
+          }
+          volume_mount {
+            mount_path = "/var/lib/docker"
+            name       = "varlibdocker"
+            read_only  = true
+          }
+          volume_mount {
+            mount_path = "/run/containerd/containerd.sock"
+            name       = "containerdsock"
+            read_only  = true
+          }
+          volume_mount {
+            mount_path = "/sys"
+            name       = "sys"
+            read_only  = true
+          }
+          volume_mount {
+            mount_path = "/dev/disk"
+            name       = "devdisk"
+            read_only  = true
+          }
+          volume_mount {
+            mount_path = "/etc/amazon-cloudwatch-observability-agent-cert"
+            name       = "agenttls"
+            read_only  = true
+          }
+        }
+        volume {
+          name = "cwagentconfig"
+          config_map {
+            name = "cwagentconfig"
+          }
+        }
+        volume {
+          name = "rootfs"
+          host_path {
+            path = "/"
+          }
+        }
+        volume {
+          name = "dockersock"
+          host_path {
+            path = "/var/run/docker.sock"
+          }
+        }
+        volume {
+          name = "varlibdocker"
+          host_path {
+            path = "/var/lib/docker"
+          }
+        }
+        volume {
+          name = "containerdsock"
+          host_path {
+            path = "/run/containerd/containerd.sock"
+          }
+        }
+        volume {
+          name = "sys"
+          host_path {
+            path = "/sys"
+          }
+        }
+        volume {
+          name = "devdisk"
+          host_path {
+            path = "/dev/disk"
+          }
+        }
+        volume {
+          name = "agenttls"
+          secret {
+            secret_name = "amazon-cloudwatch-observability-agent-cert"
+            items {
+              key  = "ca.crt"
+              path = "tls-ca.crt"
+            }
+          }
+        }
+        service_account_name             = "cloudwatch-agent"
+        termination_grace_period_seconds = 60
+      }
+    }
+  }
+}
+
+##########################################
+# Template Files
+##########################################
+locals {
+  httpd_config     = "../../../../${var.test_dir}/resources/httpd.conf"
+  httpd_ssl_config = "../../../../${var.test_dir}/resources/httpd-ssl.conf"
+  cwagent_config   = fileexists("../../../../${var.test_dir}/resources/config.json") ? "../../../../${var.test_dir}/resources/config.json" : "../default_resources/default_amazon_cloudwatch_agent.json"
 }
 
 data "template_file" "cwagent_config" {
@@ -216,7 +607,29 @@ resource "kubernetes_config_map" "cwagentconfig" {
   }
 }
 
+data "template_file" "httpd_config" {
+  template = file(local.httpd_config)
+  vars     = {}
+}
+data "template_file" "httpd_ssl_config" {
+  template = file(local.httpd_ssl_config)
+  vars     = {}
+}
 
+resource "kubernetes_config_map" "httpdconfig" {
+  depends_on = [
+    kubernetes_namespace.namespace,
+    kubernetes_service_account.cwagentservice
+  ]
+  metadata {
+    name      = "httpdconfig"
+    namespace = "amazon-cloudwatch"
+  }
+  data = {
+    "httpd.conf" : data.template_file.httpd_config.rendered
+    "httpd-ssl.conf" : data.template_file.httpd_ssl_config.rendered
+  }
+}
 
 resource "kubernetes_service_account" "cwagentservice" {
   depends_on = [kubernetes_namespace.namespace]
@@ -290,13 +703,21 @@ resource "kubernetes_cluster_role_binding" "rolebinding" {
   }
 }
 
+
 resource "null_resource" "validator" {
   depends_on = [
       kubernetes_cluster_role_binding.rolebinding,
       kubernetes_service_account.cwagentservice,
+      aws_eks_node_group.this,
+      kubernetes_daemonset.service,
       aws_eks_addon.this
   ]
   provisioner "local-exec" {
     command = "go test ${var.test_dir} -eksClusterName ${aws_eks_cluster.this.name} -computeType=EKS -v -eksDeploymentStrategy=DAEMON -eksGpuType=nvidia"
   }
 }
+locals {
+   cwagent_config   = fileexists("${var.test_dir}/resources/config.json") ? "${var.test_dir}/resources/config.json" : "../default_resources/default_amazon_cloudwatch_agent.json"
+   aws_eks  = format("%s%s", "aws eks --region ${var.region}", var.beta ? " --endpoint ${var.beta_endpoint}" : "")
+ }
+
